063cf423331f8e961d45647fc0c8d3514378df58
theweicode <theweicode@gmail.com>
2019-04-18 16:22:28 -0700

    fresh gear app in cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./assets/scss/styles.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <body>
+          <header>
+            <div class="logo">freshgear</div>
+            <div class="menu">
+              <a href="#">About</a>
+              <a href="#">New Arrival</a>
+              <a href="#">Categories</a>
+              <a href="#" class="cart-link">
+                Cart<span>4</span>
+              </a>
+            </div>
+          </header>
+          <section class="content-area home-page">
+            <div class="top-section">
+              <div class="titles">
+                <h3>Yeezy</h3>
+                <h1>Boost 350</h1>
+              </div>
+
+              <div class="product">
+                <div class="circle">
+                  <img src="/img/proudcts/adidas-yeezy-boost-350-v2.png" />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="cart-popup" class="active">
+            <div>
+              <div class="title"> My Cart</div>
+            </div>
+            <div class="cart-items">
+              <div class="item">
+                <img src="/img/proudcts/adidas-yeezy-boost-350-v2.png" />
+                <div class="delete-btn">
+                  <div class="circle">X</div>
+                </div>
+              </div>
+            </div>
+            <div class="product-total">
+              <div class="title">Total</div>
+              <div class="number">x4</div>
+            </div>
+            <div class="price-total">
+              <div class="title">Total</div>
+              <div class="number">$3,000</div>
+            </div>
+            <div class="checkout">Checkout</div>
+          </section>
+        </body>
       </div>
     );
   }
---
+++ src/assets/js/components/CartPopup.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { closingCart } from '../actions/allActions';
+
+class CartPopup extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Joe'
+		};
+	}
+	componentDidUpdate() {
+		if (this.props.globalState.popupCartOpen == true) {
+			console.log(this.props.globalState.popupCartOpen);
+			const cartPopupElement = document.getElementById('cart-popup');
+			document.addEventListener('click', event => {
+				var clickedInside = cartPopupElement.contains(event.target);
+				if (clickedInside) {
+					console.log('clicked inside');
+				} else {
+					this.props.closingCart();
+					console.log('clicked outside');
+				}
+			});
+		}
+	}
+	render() {
+		return (
+			<section
+				id="cart-popup"
+				className={this.props.globalState.popupCartOpen ? 'active' : ''}
+			>
+				<div className="cart-title">
+					<div className="title">My Cart</div>
+				</div>
+				<div className="cart-items">
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+					<div className="item-container">
+						<div className="item">
+							<img src="/img/products/adidas-yeezy-boost-350-white.png" />
+							<div className="delete-btn">
+								<div className="circle">X</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="product-total">
+					<div className="inside-container">
+						<div className="title">Total</div>
+						<div className="number"> x4</div>
+					</div>
+				</div>
+				<div className="price-total">
+					<div className="inside-container">
+						<div className="title">Total</div>
+						<div className="number"> $3,000</div>
+					</div>
+				</div>
+				<div className="checkout">
+					<div className="title">Checkout</div>
+					<span className="ti-shopping-cart" />
+				</div>
+			</section>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return state;
+};
+
+export default connect(
+	mapStateToProps,
+	{
+		closingCart
+	}
+)(CartPopup);
